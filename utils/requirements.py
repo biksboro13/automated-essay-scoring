99e6198c3798efc305f768a82f587c03e487c262
Bikash Boro <itsmebikash13@gmail.com>
2019-05-26 11:46:55 +0530

    commiting

---
+++ utils/requirements.py
@@ -0,0 +1,22 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+import nltk
+import re
+from nltk.corpus import wordnet as wn
+from nltk.corpus import stopwords
+from nltk.tokenize import wordpunct_tokenize
+import grammar_check
+from collections import Counter
+import textmining
+from time import time
+import matplotlib.pyplot as plt
+from sklearn.model_selection import KFold
+from sklearn.linear_model import LinearRegression
+from metrics import kappa
+import logging
+from gensim.models import word2vec
+
+from keras.layers import Embedding, LSTM, Dense, Dropout, Lambda, Flatten
+from keras.models import Sequential, load_model, model_from_config
+
